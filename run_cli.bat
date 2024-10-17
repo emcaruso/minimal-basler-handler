e078d1d03fd0543a9e15adb3b409a3fed65ced24
Caruso Emanuele (Student NaTec PhD22) <emcaruso@unibz.it>
2024-10-18 00:36:03 +0200

    windows .bat scripts

---
+++ run_cli.bat
@@ -0,0 +1,17 @@
+@echo off
+set SCRIPTPATH=%~dp0
+
+REM Check if the virtual environment directory exists
+if exist "%SCRIPTPATH%env\.venv\" (
+    echo Environment found. Activating...
+    call "%SCRIPTPATH%env\.venv\Scripts\activate.bat"
+) else (
+    echo Environment not found. Creating a new one...
+    python -m venv "%SCRIPTPATH%env\.venv"
+    call "%SCRIPTPATH%env\.venv\Scripts\activate.bat"
+    pip install -r "%SCRIPTPATH%env\requirements.txt"
+)
+
+REM Execute the server
+cd src
+python user_shell.py
